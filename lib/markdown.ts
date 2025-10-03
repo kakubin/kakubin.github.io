fc3a7069e0f30d17cf3d1cee25caaf39d6bfdeb3
kakubin <kakubin@users.noreply.github.com>
2025-10-03 09:51:45 +0900

    update marked@10.0.0

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,15 +1,15 @@
-import prism from 'prismjs'
+// import prism from 'prismjs'
 import { marked } from 'marked'
 
-marked.setOptions({
-  highlight: function(code, lang) {
-    if (prism.languages[lang]) {
-      return prism.highlight(code, prism.languages[lang], lang);
-    } else {
-      return code;
-    }
-  },
-  langPrefix: 'language-',
-});
+// marked.setOptions({
+//   highlight: function(code, lang) {
+//     if (prism.languages[lang]) {
+//       return prism.highlight(code, prism.languages[lang], lang);
+//     } else {
+//       return code;
+//     }
+//   },
+//   langPrefix: 'language-',
+// });
 
 export const parseMarkdown = (content: string) => ({ __html: marked.parse(content) })
