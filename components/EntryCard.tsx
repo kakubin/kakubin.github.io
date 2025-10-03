cee2ec95162e0f9f8a4b981e77f25a299fb6da9a
kakubin <kakubin@users.noreply.github.com>
2025-10-03 12:40:02 +0900

    static link

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link'
-
 const EntryCard = (props: EntryMetaData) => {
   return (
-    <Link href={`/entry/${props.date}`}>
+    <a href={`/entry/${props.date}`}>
       <div className="entry-card">
         <div className="entry-date">
           <div className="date-text">{props.date}</div>
@@ -11,7 +9,7 @@ const EntryCard = (props: EntryMetaData) => {
           <div className="entry-text">{props.title}</div>
         </div>
       </div>
-    </Link>
+    </a>
   )
 }
 
